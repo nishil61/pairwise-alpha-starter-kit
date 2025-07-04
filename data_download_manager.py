e36a7c1bc17354c9a0831605aba1a668c7966f96
Abhijeet Shrivastava <142328838+CodeforAbj@users.noreply.github.com>
2025-07-04 17:16:53 +0530

    Update data_download_manager.py date range

--- data_download_manager.py
+++ data_download_manager.py
@@ -29,9 +29,9 @@ class CryptoDataManager:
         self.rate_limit_delay = 0.1
         self.max_retries = 3
         
-        # Fixed date range
-        self.start_date = "2024-06-01 00:00:00"
-        self.end_date = "2025-05-31 23:59:59"
+        # Round 3 date range
+        self.start_date = "2025-01-01 00:00:00"
+        self.end_date = "2025-06-29 23:59:59"
         
         # Binance interval mapping
         self.interval_map = {
